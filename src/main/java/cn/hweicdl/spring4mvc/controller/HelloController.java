b65ab2ce0af8127e4c5033f40d10ee01bd24b975
Wei Huang <hweicdl@cn.ibm.com>
2014-04-09 22:29:38 +0800

    first commit

---
+++ src/main/java/cn/hweicdl/spring4mvc/controller/HelloController.java
@@ -0,0 +1,17 @@
+package cn.hweicdl.spring4mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "hello"; // return to ${prefix}/hello${suffix}
+	}
+}
